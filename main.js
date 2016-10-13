167f135b6d3c4cd0069445b8f2e481a6a27e7574
Barry Howard <BarryLeeHoward@gmail.com>
2016-10-13 17:34:17 -0400

    Styling

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-console.log("loaded");
-
-$(".grid").html("Howdy Howdy");
-
+var numOfDisp = 16;
 var results = data.results;
-console.log(results);
-var test = "";
+var gridHTML = "";
 
-function addBlock (item) {
-
-	var img = item.Images[0].url_fullxfull;
+function addBlock (index) {
+	var item = results[index];
+	var img = item.Images[0].url_170x135;
 	var title = item.title;
 	var author = item.Shop.shop_name;
 	var price = item.price;
-	return `<div class="block"><img src="${img}"><p>${title}</p><p>${author}</p><p>${price}</p></div>`;
-
+	var urlTop = `<a class="block-link" href="${item.url}"><div class="block"><img src="${img}">`;
+	var urlBottom = `<p>${title}</p><span class="author">${author}</span><span class="price">${price}</span></div></a>`;
+	return `${urlTop}${urlBottom}`;
 }
 
-results.forEach(function(element){
-	var newHTML = addBlock(element);
-	test = `${test}${newHTML}`;
-})
+for (i=0; i<numOfDisp; i++){
+	var newHTML = addBlock(i);
+	gridHTML = `${gridHTML}${newHTML}`;
+}
 
-$(".grid").html(test);
+$(".search-bar").attr("placeholder", `${data.params.keywords}`);
+$("#All-Categories").html(`<p>All categories > "${data.params.keywords}" (${data.count} Results)</p>`);
+$(".grid").html(gridHTML);
